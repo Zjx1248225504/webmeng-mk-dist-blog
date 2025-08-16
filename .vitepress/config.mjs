ee5f6dfbe69912f1e495227da4b71709a12e0a68
eric_ <zhangjunxiang@vzan.com>
2025-08-16 16:16:57 +0800

    feat: updata-test

---
+++ .vitepress/config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "webmeng-md-blog",
+  description: "使用本地node.js MK编辑器 + git + vite MK转html + 热铁盒自动部署",
+  base: '/vite-press/public/dist/',
+  // build: {
+  //   outDir: '../public/docs', // 自定义输出路径（相对或绝对路径）
+  // },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'gitee', link: 'https://gitee.com/Zjxm/webmeng-mk-blog' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "webmeng-md-blog"
+  text: "使用本地node.js MK编辑器 + git + vite MK转html + 热铁盒自动部署"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
